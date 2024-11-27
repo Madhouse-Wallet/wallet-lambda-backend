48655804acbfd3bedb347085220f65b5f7dcdb10
Gauravk0029 <gaurav@suffescom.co>
2024-11-27 19:27:01 +0530

    added new env

--- db.js
+++ db.js
@@ -4,18 +4,24 @@ const mongoose = require("mongoose");
 let isConnected;
 
 const connectToDatabase = async () => {
-  console.log("process.env.MONGODB_URI",process.env.MONGODB_URI)
-  if (isConnected) {
-    console.log("=> Using existing database connection");
-    return;
+  try {
+    console.log("process.env.MONGODB_URI", process.env.MONGODB_URI, isConnected)
+    if (isConnected) {
+      console.log("=> Using existing database connection");
+      return;
+    }
+
+    console.log("=> Establishing new database connection");
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("readyState", mongoose.connection.readyState)
+    isConnected = mongoose.connection.readyState;
+  } catch (e) {
+    console.log("error-->", e)
   }
 
-  console.log("=> Establishing new database connection");
-  await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  isConnected = mongoose.connection.readyState;
 };
 
 module.exports = connectToDatabase;
